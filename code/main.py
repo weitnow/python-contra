b02ef3c133b62cb86258d6e9dd93156ccb958d57
weitnow <weitnow@gmail.com>
2023-08-02 15:35:20 +0200

    initial commit

---
+++ code/main.py
@@ -0,0 +1,73 @@
+import pygame, sys
+from settings import *
+from pytmx.util_pygame import load_pygame
+from tile import Tile
+from player import Player
+from pygame.math import Vector2 as vector
+
+
+class AllSprites(pygame.sprite.Group):
+    def __init__(self):
+        super().__init__()
+        self.display_surface = pygame.display.get_surface()
+        self.offset = vector()
+
+    def custom_draw(self, player):
+        # change the offset vector
+        self.offset.x = player.rect.centerx - WINDOW_WIDTH / 2
+        self.offset.y = player.rect.centery - WINDOW_HEIGHT / 2
+
+        # blit all sprites
+        for sprite in sorted(self.sprites(), key = lambda sprite: sprite.z):
+            offset_rect = sprite.image.get_rect(center=sprite.rect.center)
+            offset_rect.center -= self.offset
+            self.display_surface.blit(sprite.image, offset_rect)
+
+
+class Main:
+    def __init__(self):
+        pygame.init()
+        self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+        pygame.display.set_caption('Contra')
+        self.clock = pygame.time.Clock()
+
+        # groups
+        self.all_sprites = AllSprites()
+        self.setup()
+
+    def setup(self):
+        tmx_map = load_pygame('../data/map.tmx')
+
+        # tiles
+        for x, y, surf in tmx_map.get_layer_by_name('Level').tiles():
+            Tile((x * 64, y * 64), surf, self.all_sprites, LAYERS['Level'])
+
+        for layer in ['BG', 'BG Detail', 'FG Detail Bottom', 'FG Detail Top']:
+            for x, y, surf in tmx_map.get_layer_by_name(layer).tiles():
+                Tile((x * 64, y * 64), surf, self.all_sprites, LAYERS[layer])
+
+        # objects
+        for obj in tmx_map.get_layer_by_name("Entities"):
+            if obj.name == 'Player':
+                self.player = Player((obj.x, obj.y), self.all_sprites, '../graphics/player')
+
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+            dt = self.clock.tick() / 1000
+            self.display_surface.fill((249, 131, 103))
+
+            self.all_sprites.update(dt)
+            # self.all_sprites.draw(self.display_surface)
+            self.all_sprites.custom_draw(self.player)
+
+            pygame.display.update()
+
+
+if __name__ == '__main__':
+    main = Main()
+    main.run()
---
+++ code/player.py
@@ -0,0 +1,72 @@
+import pygame
+from settings import *
+from pygame.math import Vector2 as vector
+from os import walk
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, pos, groups, path):
+        super().__init__(groups)
+        self.import_assets(path)
+        self.frame_index = 0
+        self.status = 'right'
+        self.image = self.animations[self.status][self.frame_index]
+
+        self.rect = self.image.get_rect(topleft=pos)
+        self.z = LAYERS['Level']
+
+        self.direction = vector()
+        self.pos = vector(self.rect.topleft)
+        self.speed = 400
+
+
+    def import_assets(self, path):
+        self.animations = {}
+        for index, folder in enumerate(walk(path)):
+            if index == 0:
+                for name in folder[1]:
+                    self.animations[name] = []
+            else:
+                for file_name in sorted(folder[2], key=lambda string: int(string.split('.')[0])):
+                    path = folder[0].replace('\\', '/') + '/' + file_name
+                    surf = pygame.image.load(path).convert_alpha()
+                    key = folder[0].split('/')[3]
+                    self.animations[key].append(surf)
+
+    def animate(self, dt):
+        self.frame_index += 7 * dt
+        if self.frame_index >= len(self.animations[self.status]):
+            self.frame_index = 0
+
+        self.image = self.animations[self.status][int(self.frame_index)]
+
+    def input(self):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_d]:
+            self.direction.x = 1
+        elif keys[pygame.K_a]:
+            self.direction.x = -1
+        else:
+            self.direction.x = 0
+
+        if keys[pygame.K_w]:
+            self.direction.y = -1
+        elif keys[pygame.K_s]:
+            self.direction.y = 1
+        else:
+            self.direction.y = 0
+
+    def move(self, dt):
+        # horizontal movement
+        self.pos.x += self.direction.x * self.speed * dt
+        self.rect.x = round(self.pos.x)
+
+        # vertical movement
+        self.pos.y += self.direction.y * self.speed * dt
+        self.rect.y = round(self.pos.y)
+
+    def update(self, dt):
+        self.input()
+        self.move(dt)
+        self.animate(dt)
