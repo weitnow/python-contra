22e4a71f12e8a0fc5836efca2edd7af6fc69ce7a
weitnow <weitnow@gmail.com>
2023-08-16 20:28:32 +0200

    enemy and shoot ability implemented

--- code/main.py
+++ code/main.py
@@ -5,6 +5,7 @@ from tile import Tile, CollisionTile, MovingPlatform
 from player import Player
 from pygame.math import Vector2 as vector
 from bullet import Bullet, Fireanimation
+from enemy import Enemy
 
 class AllSprites(pygame.sprite.Group):
     def __init__(self):
@@ -78,6 +79,9 @@ class Main:
         for obj in tmx_map.get_layer_by_name("Entities"):
             if obj.name == 'Player':
                 self.player = Player((obj.x, obj.y), self.all_sprites, '../graphics/player', self.collision_sprites, self.shoot)
+            if obj.name == 'Enemy':
+                Enemy((obj.x, obj.y), '../graphics/enemies', self.all_sprites, self.shoot, self.player, self.collision_sprites)
+
 
         self.platform_border_rects = []
         for obj in tmx_map.get_layer_by_name('Platforms'):
